7f0dacfcc86890c920ee16b9569d06eccb62a2f5
MichaelVasiliev24 <mv82374@gmail.com>
2025-11-07 17:21:31 +0300

    Fix: Test Extension

--- test/test_checksum.py
+++ test/test_checksum.py
@@ -1,9 +1,21 @@
-from src.checksum import modulo11Checksum
+import pytest
+
+from src.checksum import modulo_11_checksum
 
 
 def test_good():
-    assert modulo11Checksum("2-266-11156-8")
+    assert modulo_11_checksum("2-266-11156-8")
 
 
 def test_bad():
-    assert not modulo11Checksum("2-266-11156-3")
+    assert not modulo_11_checksum("2-266-11156-3")
+
+
+def test_less_chars():
+    with pytest.raises(Exception):
+        modulo_11_checksum("2 35")
+
+
+def test_inappropriate_chars():
+    with pytest.raises(Exception):
+        modulo_11_checksum("dfgdfg")
